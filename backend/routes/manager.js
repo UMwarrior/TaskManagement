c0b1e933ef3278dc00e6132823744c148e78e0b0
Uday Madan <udaymadan24@gmail.com>
2024-01-04 15:34:36 +0530

    Manager APIs ADDED

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -22,55 +22,6 @@ router.post('/login', (req, res) => {
 });
 
 
-// router.get('/all/employee', authenticateSuperAdmin, (req, res) => {
-//     db.query('SELECT * FROM employees', (err, results) => {
-//         if (err) throw err;
-//         res.json(results);
-//     });
-// });
-
-// router.post('/add/employee', authenticateSuperAdmin, (req, res) => {
-//     try {
-//         const { employee_name, phone_number, password } = req.body; // Assuming you send 'admin' flag in the request body
-
-//         if (!employee_name || !phone_number || !password) {
-//             // Check if required fields are missing
-//             return res.status(400).json({ error: 'Missing required fields' });
-//         }
-
-//         db.query('INSERT INTO employees (employee_name, phone_number, password) VALUES (?, ?, ?)', [employee_name, phone_number, password], (err, result) => {
-//             if (err) {
-//                 console.error('Error inserting employee:', err);
-//                 return res.status(500).json({ error: 'Internal Server Error' });
-//             }
-
-//             res.json({ employee_id: result.insertId, employee_name, phone_number });
-//         });
-//     } catch (error) {
-//         console.error('Unexpected error:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-// router.put('/update/employee/:employeeId', authenticateSuperAdmin, (req, res) => {
-//     const employeeId = req.params.employeeId;
-//     const { employee_name, phone_number, password, admin } = req.body;
-//     db.query('UPDATE employees SET employee_name = ?, phone_number = ?, password = ?, admin = ? WHERE employee_id = ?', [employee_name, phone_number, password, admin, employeeId], (err) => {
-//         if (err) throw err;
-//         res.json({ message: 'Employee updated successfully' });
-//     });
-// });
-
-// // Delete employee by ID
-// router.delete('/delete/employee/:employeeId', authenticateSuperAdmin, (req, res) => {
-//     const employeeId = req.params.employeeId;
-//     db.query('DELETE FROM employees WHERE employee_id = ?', [employeeId], (err) => {
-//         if (err) throw err;
-//         res.json({ message: 'Employee deleted successfully' });
-//     });
-// });
-
-
 router.get('/all/project', authenticateManager, (req, res) => {
     db.query('SELECT * FROM projects WHERE manager_id = ?', [req.employeeId], (err, results) => {
         if (err) throw err;
@@ -100,70 +51,156 @@ router.get('/project/:projectId', authenticateManager, (req, res) => {
         }
     })
 })
-// router.post('/add/project', authenticateSuperAdmin, (req, res) => {
 
-//     try {
-//         const { manager_id, normal_employee_ids, project_name, project_description } = req.body;
+router.post('/create/task' , authenticateManager , (req,res) => {
+    const {project_id , employee_id , duration , task_description} = req.body;
 
-//         if (!manager_id || !normal_employee_ids || !project_name || !project_description) {
-//             // Check if required fields are missing
-//             return res.status(400).json({ error: 'Missing required fields' });
-//         }
+    if (!project_id || !employee_id || !duration || !task_description) {
+        res.status(400).json({message: "Required Feilds are Missing"});
+    }
 
-//         db.query('INSERT INTO projects (manager_id, project_name, project_description) VALUES (?, ?, ?)', [manager_id, project_name, project_description], (err, result) => {
-//             if (err) throw err;
+    date = new Date();
+    end_date = new Date(date.setDate(date.getDate() + duration));
+    start_date = new Date();
 
-//             const projectId = result.insertId;
+    db.query('SELECT * FROM projects WHERE project_id = ? and manager_id = ?' , [project_id , req.employeeId] , (err , results)=>{
+        if(err) throw err;
 
-//             res.json({ project_id: projectId, manager_id, normal_employee_ids, project_name });
+        if(results.length>0){
+            db.query('SELECT * FROM project_employees WHERE project_id = ? and employee_id = ?' , [project_id , employee_id] , (err1 , results1)=>{
+                if(err1) throw err1;
+        
+                if(results1.length>0){
+                    db.query('INSERT INTO project_tasks (combination_id , status , assigning_date , last_date , task_description) VALUES (?,?,?,?,?)' , [results1[0].combination_id , 0 , start_date, end_date, task_description ] , (err2 , results2)=>{
+                        if(err2) throw err2;
+        
+                        res.json({task_id:results2.insertId , project_id, employee_id, start_date, end_date});
+                    })
+                }
+            })
+        }
+    })
+})
 
-//             const normalEmployeeValues = normal_employee_ids.map(employeeId => [projectId, employeeId]);
-//             db.query('INSERT INTO project_employees (project_id, employee_id) VALUES ?', [normalEmployeeValues], (err) => {
-//                 if (err) throw err;
-//             });
-//         });
-//     } catch (error) {
-//         console.error('Unexpected error:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
+router.get('/all/task', authenticateManager, (req, res) => {
+    const {project_id} = req.body;
 
-// // Update project by ID (Super Admin only)
-// router.put('/update/project/:projectId', authenticateSuperAdmin, (req, res) => {
-//     const projectId = req.params.projectId;
-//     const { manager_id, normal_employee_ids, project_name, project_description } = req.body;
+    if (!project_id) {
+        res.status(400).json({message: "Required Feilds are Missing"});
+    }
 
-//     // Update project details in projects table
-//     db.query('UPDATE projects SET manager_id = ?, project_name = ?, project_description = ? WHERE project_id = ?', [manager_id, project_name, project_description, projectId], (err) => {
-//         if (err) throw err;
+    db.query('SELECT * FROM projects WHERE project_id = ? and manager_id = ?' , [project_id , req.employeeId] , (err , results)=>{
+        if(err) throw err;
 
-//         // Update normal employees associated with the project in project_employees table
-//         const normalEmployeeValues = normal_employee_ids.map(employeeId => [projectId, employeeId]);
-//         db.query('DELETE FROM project_employees WHERE project_id = ?', [projectId], (err) => {
-//             if (err) throw err;
+        let tasks = [];
 
-//             db.query('INSERT INTO project_employees (project_id, employee_id) VALUES ?', [normalEmployeeValues], (err) => {
-//                 if (err) throw err;
-//                 res.json({ message: 'Project updated successfully' });
-//             });
-//         });
-//     });
-// });
+        if(results.length>0){
+            db.query('SELECT * FROM project_employees WHERE project_id = ? ' , [project_id] , (err1 , results1)=>{
+                if(err1) throw err1;
+        
+                if(results1.length>0){
+                    for (let i = 0; i < results1.length; i++) {
+                        db.query('SELECT * FROM project_tasks WHERE combination_id = ? ' , [results1[i].combination_id] , (err2 , results2)=>{
+                            if(err2) throw err2;
+                    
+                            if(results2.length>0){
+                                for (let j = 0; j < results2.length; j++) {
+                                    results2[j].employee_id = results1[i].employee_id;
+                                    tasks.push(results2[j]);
+                                }
+                            }
+                            if (results1.length-1 === i) {
+                                res.json({tasks});
+                            }
+                        })
+                    }
+                }
+            })
+        }else{
+            res.status(403).json({message: "Access Denied"});
+        }
+    })
+});
 
-// // Delete project by ID (Super Admin only)
-// router.delete('/delete/project/:projectId', authenticateSuperAdmin, (req, res) => {
-//     const projectId = req.params.projectId;
+router.get('/completed/task', authenticateManager, (req, res) => {
+    const {project_id} = req.body;
 
-//     // Delete project from projects table
-//     db.query('DELETE FROM projects WHERE project_id = ?', [projectId], (err) => {
-//         if (err) throw err;
+    if (!project_id) {
+        res.status(400).json({message: "Required Feilds are Missing"});
+    }
 
-//         // Also, delete entries from project_employees table related to the project
-//         db.query('DELETE FROM project_employees WHERE project_id = ?', [projectId], (err) => {
-//             if (err) throw err;
-//             res.json({ message: 'Project deleted successfully' });
-//         });
-//     });
-// });
+    db.query('SELECT * FROM projects WHERE project_id = ? and manager_id = ?' , [project_id , req.employeeId] , (err , results)=>{
+        if(err) throw err;
+
+        let tasks = [];
+
+        if(results.length>0){
+            db.query('SELECT * FROM project_employees WHERE project_id = ? ' , [project_id] , (err1 , results1)=>{
+                if(err1) throw err1;
+        
+                if(results1.length>0){
+                    for (let i = 0; i < results1.length; i++) {
+                        db.query('SELECT * FROM project_tasks WHERE combination_id = ? and status = 1' , [results1[i].combination_id] , (err2 , results2)=>{
+                            if(err2) throw err2;
+                    
+                            if(results2.length>0){
+                                for (let j = 0; j < results2.length; j++) {
+                                    results2[j].employee_id = results1[i].employee_id;
+                                    tasks.push(results2[j]);
+                                }
+                            }
+                            if (results1.length-1 === i) {
+                                res.json({tasks});
+                            }
+                        })
+                    }
+                }
+            })
+        }else{
+            res.status(403).json({message: "Access Denied"});
+        }
+    })
+});
+
+router.get('/pending/task', authenticateManager, (req, res) => {
+    const {project_id} = req.body;
+
+    if (!project_id) {
+        res.status(400).json({message: "Required Feilds are Missing"});
+    }
+
+    db.query('SELECT * FROM projects WHERE project_id = ? and manager_id = ?' , [project_id , req.employeeId] , (err , results)=>{
+        if(err) throw err;
+
+        let tasks = [];
+
+        if(results.length>0){
+            db.query('SELECT * FROM project_employees WHERE project_id = ? ' , [project_id] , (err1 , results1)=>{
+                if(err1) throw err1;
+        
+                if(results1.length>0){
+                    for (let i = 0; i < results1.length; i++) {
+                        db.query('SELECT * FROM project_tasks WHERE combination_id = ? and status = 0' , [results1[i].combination_id] , (err2 , results2)=>{
+                            if(err2) throw err2;
+                    
+                            if(results2.length>0){
+                                for (let j = 0; j < results2.length; j++) {
+                                    results2[j].employee_id = results1[i].employee_id;
+                                    tasks.push(results2[j]);
+                                }
+                            }
+                            if (results1.length-1 === i) {
+                                res.json({tasks});
+                            }
+                        })
+                    }
+                }
+            })
+        }else{
+            res.status(403).json({message: "Access Denied"});
+        }
+    })
+});
+
 
 module.exports = router;
